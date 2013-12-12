162a378ae78a5281231f6886b170f9fddeadf2e2
Alexandre BRAULT <abrault@e2r7p7.42.fr>
2013-12-12 18:28:41 +0100

    Maj du fdf

--- libft.h
+++ libft.h
@@ -6,36 +6,44 @@
 /*   By: abrault <abrault@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 15:17:35 by abrault           #+#    #+#             */
-/*   Updated: 2013/11/29 12:36:57 by abrault          ###   ########.fr       */
+/*   Updated: 2013/12/12 12:20:03 by abrault          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef		FT_LIBFT_H
 # define	FT_LIBFT_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <X11/Xos.h>
-#include <X11/Xatom.h>
-#include <X11/keysym.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
 
-#define MIN(a,b) (a < b ? a : b)
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
 
-int		ft_atoi(const char *str);
-int		ft_strlen(const char *str);
-int		ft_isalnum(int n);
-int		ft_isalpha(int n);
-int		ft_isascii(int n);
-int		ft_isdigit(int n);
+# include <X11/Xlib.h>
+# include <X11/Xutil.h>
+# include <X11/Xos.h>
+# include <X11/Xatom.h>
+# include <X11/keysym.h>
+
+# define BUFF_SIZE 1000
+# define MIN(a,b) (a < b ? a : b)
+
+/* Fonction Generale */
+
+int			ft_atoi(const char *str);
+int			ft_strlen(const char *str);
+int			ft_isalnum(int n);
+int			ft_isalpha(int n);
+int			ft_isascii(int n);
+int			ft_isdigit(int n);
 int 		ft_isprint(int n);
-int		ft_strcmp(const char *str1, const char *str2);
-int		ft_strncmp(const char *str1, const char *str2, size_t n);
-int		ft_tolower(int n);
-int		ft_toupper(int n);
-int		ft_memcmp(const void *ptr1, const void *ptr2, size_t num);
+int			ft_strcmp(const char *str1, const char *str2);
+int			ft_strncmp(const char *str1, const char *str2, size_t n);
+int			ft_tolower(int n);
+int			ft_toupper(int n);
+int			ft_memcmp(const void *ptr1, const void *ptr2, size_t num);
 
 void		ft_bzero(void *s, size_t n);
 void		*ft_memset(void *ptr, int charac, int num);
@@ -47,18 +55,32 @@ void		*ft_strdup(const char *str);
 void		ft_putchar(char c);
 void		ft_putstr(const char *str);
 void		ft_putendl(const char *s);
+void		ft_getTab2(int ***tab, int height, int width);
 
 char		*ft_strchr(const char *str, int c_ascii);
 char		*ft_strcat(char *dest, const char *src);
 char		*ft_strncat(char *dest, const char *src, size_t n);
 char		*ft_strcpy(char *destination, const char *source);
-char		*ft_strncpy(char *destination, const char *source, size_t size);
+char		*ft_strncpy(char *desti, const char *source, size_t size);
 char		*ft_strnstr(const char *str_1, const char *str_2, size_t n);
 char		*ft_strrchr(const char *str, int c_ascii);
 char		*ft_strstr(const char *str_1, const char *str_2);
 
 size_t		ft_strlcat(char *dest, const char *src, size_t n);
 
-void    	mlx_CreateWindow(Display **dis, Window *win, int height, int width);
-GC		mlx_getColor(Display *dis, Window win, char *color);
+/* Gestion des commandes entree standard */
+int			getCommand(int argc, char **argv, char **option);
+
+/* Fonction propre a mlx */
+
+void    	mlx_CreateWindow(Display **dis, Window *win, int h, int w);
+GC			mlx_getColor(Display *dis, Window win, char *color);
+
+/* Fonction gestion de fichier */
+
+int			get_next_line(int const fd, char **line);
+char		*extract_line(char **str);
+char		*re_malloc(char *buff, char *str);
+int			traitement(char **stock, char **line);
+
 #endif
--- main.c
+++ main.c
@@ -6,47 +6,89 @@
 /*   By: abrault <abrault@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/28 09:41:45 by abrault           #+#    #+#             */
-/*   Updated: 2013/11/28 17:33:08 by abrault          ###   ########.fr       */
+/*   Updated: 2013/12/12 14:17:29 by abrault          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "libft.h"
 
-void	BuildAllSegment(int tab[12][19], Display *dis, Window win)
+int		getVal(char *file, int x, int y, char *c)
+{
+	int		_x;
+	int		_y;
+	int		fd;
+	char	*line;
+	char	*tok;
+
+	_y = 0;
+	x++;
+	y++;
+	fd = open(file, O_RDONLY);
+	while (_y < y)
+	{
+		_x = 0;
+		get_next_line(fd, &line);
+		tok = strtok(line, " ");
+		while (_x < x)
+		{
+			tok = strtok(NULL, " ");
+			printf(" %s |", tok);
+			if (ft_strcmp(line, "\n") == 0)
+				*c = '\n';
+			if (ft_strcmp(line, "\0") == 0)
+				*c = '\0';
+			_x++;
+		}
+		_y++;
+	}
+	close(fd);
+	return (ft_atoi(tok));
+}
+
+void	BuildAllSegment(char *file, Display *dis, Window win)
 {
 	int		x;
 	int		y;
 	int		q;
+	int		val[3];
+	char	c;
 
+	c = '0';
 	y = 0;
 	q = 0;
-	while (y < 11)
+	while (c != '\0')
 	{
 		x = 0;
-		while (x < 19)
+		while (1)
 		{
-			if ((tab[y][x] > 0 || tab[y + 1][x] > 0 ||
-			tab[y][x + 1] > 0 || (tab[y + 1][x] < tab[y][x]))
-			&& y < 11)
+			printf("x = %d\n", x);
+			val[0] = getVal(file, x, y, &c);
+			if (c == '\n')
+				break ;
+			val[1] = getVal(file, x + 1, y, &c);
+			val[2] = getVal(file, x, y + 1, &c);
+			if (((val[0] > 0) || (val[2] > 0) ||
+			(val[1] > 0) || (val[2] < val[0])) && y < 11)
 				XDrawLine(dis, win, mlx_getColor(dis, win,
 				"#FF0000"),(x + 10) * 25 - (q * 20), (y + 2)
-				* 25 - tab[y][x] * 2, (x + 10 + 1) * 25 -
-				(q * 20), (y + 2) * 25 - tab[y][x + 1] * 2);
+				* 25 - val[0] * 2, (x + 10 + 1) * 25 -
+				(q * 20), (y + 2) * 25 - val[1] * 2);
 			else if (x < 18)
 				XDrawLine(dis, win, mlx_getColor(dis, win,
 				"#FEFEFE"),(x + 10) * 25 - (q * 20), (y + 2)
-				* 25 - tab[y][x] * 2, (x + 11) * 25 -
-				(q * 20), (y + 2) * 25 - tab[y][x + 1] * 2);
-			if (y <  10 && (tab[y + 1][x] > 0 || tab[y][x] > 0))
+				* 25 - val[0] * 2, (x + 11) * 25 -
+				(q * 20), (y + 2) * 25 - val[1] * 2);
+			if (y <  10 && (val[2] > 0 || val[0] > 0))
 				XDrawLine(dis, win, mlx_getColor(dis, win,
 				"#FF0000"), (x + 10) * 25 - (q * 20), (y + 2)
-				* 25 - tab[y][x] * 2, (x + 10) * 25 - ((q + 1)
-				* 20), (y + 3) * 25 - tab[y + 1][x] * 2);
+				* 25 - val[0] * 2, (x + 10) * 25 - ((q + 1)
+				* 20), (y + 3) * 25 - val[2] * 2);
 			else if (y <  10)
 				XDrawLine(dis, win, mlx_getColor(dis, win,
 				"#FEFEFE"), (x + 10) * 25 - (q * 20), (y + 2)
-				* 25 - tab[y][x] * 2, (x + 10) * 25 - ((q + 1)
-				* 20), (y + 3) * 25 - tab[y + 1][x] * 2);
+				* 25 - val[0] * 2, (x + 10) * 25 - ((q + 1)
+				* 20), (y + 3) * 25 - val[2] * 2);
 			x++;
 		}
 		q++;
@@ -54,31 +96,22 @@ void	BuildAllSegment(int tab[12][19], Display *dis, Window win)
 	}
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	int		tab[12][19] ={
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,10,10,0,0,10,10,0,0,0,10,10,10,10,10,0,0,0},
-	{0,0,10,10,0,0,10,10,0,0,0,0,0,0,0,10,10,0,0},
-	{0,0,10,10,0,0,10,10,0,0,0,0,0,0,0,10,10,0,0},
-	{0,0,10,10,10,10,10,10,0,0,0,0,10,10,10,10,0,0,0},
-	{0,0,0,10,10,10,10,10,0,0,0,10,10,0,0,0,0,0,0},
-	{0,0,0,0,0,0,10,10,0,0,0,10,10,0,0,0,0,0,0},
-	{0,0,0,0,0,0,10,10,0,0,0,10,10,10,10,10,10,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
 	Display		*dis;
 	Window		win;
 	XEvent		e;
 
+	argc++;
+	char		c;
+
+	c = '0';
 	mlx_CreateWindow(&dis, &win, 350, 750);
 	while (1)
 	{
 		XNextEvent(dis, &e);
 		if (e.type == Expose)
-			BuildAllSegment(tab, dis, win);
+			BuildAllSegment(argv[1], dis, win);
 		if (e.type == KeyPress)
 		{
 			if ((int) XLookupKeysym (&e.xkey, 0) == 65307)
